dede23c661a744d0e77375ede6381143f0d05a4b
Madeline Legate <136637960+mmlegate@users.noreply.github.com>
2023-08-28 14:09:25 -0700

    Another summary

--- main.py
+++ main.py
@@ -40,49 +40,60 @@ def get_deck():
             deck[i][3] = 2
         # Shuffling
     np.random.shuffle(deck)
-    return deck[:12], deck
+    return deck
 
 
-def find_set(v):
-    set = []
+def find_set(subsets):
+    # v is the set of all combinations of a group of cards
+    indices = []
+    sets_indices = []
+    sets = []
+    v = subsets.copy()
     for i in range(len(v)):
-        for j in range(3):
-            if v[i][0][j] == v[i][1][j] and v[i][0][j] != v[i][2][j]:
-                break
-            elif v[i][0][j] == v[i][2][j] and v[i][0][j] != v[i][1][j]:
-                break
-            elif v[i][2][j] == v[i][1][j] and not v[i][2][j] != v[i][0][j]:
-                break
+        for j in range(4):
+            if (v[i][0][j] == v[i][1][j] == v[i][2][j]) or (v[i][0][j] != v[i][1][j] and v[i][0][j] != v[i][2][j]):
+                indices = np.append(indices, i)
             else:
-                set = np.append(set, v[i])
-    return set
+                break
+        if len(indices) >= 4:
+            for i in range(len(indices) - 4):
+                if indices[i] == indices[i + 1] == indices[i + 2] == indices[i + 3]:
+                    sets_indices = np.append(sets_indices, indices[i])
+        sets_indices = np.unique(sets_indices)
+        for i in range(len(sets_indices)):
+            sets = np.append(sets, v[int(sets_indices[i])])
+        sets = np.reshape(sets, (int(len(sets) / 12), 3, 4))
+    return sets
 
 
-def get_spread(v, deck):
+def get_spread(deck):
+    v = deck[:12]
+    deck_new = deck[12:]
     subsets = list(combo(v, 3))
     sets = find_set(subsets)
     if len(sets) == 0:
         v = np.append(v, deck[:3])
-        deck = deck[3:]
+        deck_new = deck_new[3:]
         subsets = list(combo(v, 3))
         sets = find_set(subsets)
         if len(sets) == 0:
             v = np.append(v, deck[:3])
-            deck = deck[3:]
+            deck_new = deck_new[3:]
             subsets = list(combo(v, 3))
             sets = find_set(subsets)
-    index = []
-    for i in range(len(v)):
-        if v[i] == sets[0][0] or v[i] == sets[0][1] or v[i] == sets[0][2]:
-            index = np.append(index, i)
-        elif len(index) >= 3:
-            break
-    for i in range(len(index)):
-        v[index[i]] = deck[i]
-    deck = deck[3:]
-    if len(deck) <= 18 and len(sets) == 0:
-        return "done"
-    get_spread(v, deck)
 
+    key = sets[0]
+    indices = []
+    for i in range(3):
+        for j in range(len(v)):
+            for k in range(4):
+                if key[i][k] == v[j][k]:
+                    indices = np.append(indices, j)
+    for k in range(len(indices)):
+        v[int(indices[k])] = deck_new[k]
+    new_deck = np.append(v, deck_new[len(indices):])
 
-get_spread(get_deck()[0], get_deck()[1])
+    if len(deck_new[len(indices):]) <= 18 and len(sets) == 0:
+        return "Yay!"
+    get_spread(new_deck)
+get_spread(get_deck())
